04858118263b508f280b6cace947fec591626e18
Rylan Boothman <rylan@nia-login02.scinet.local>
2018-06-08 15:04:30 -0400

    Simplify script, now correctly works on small inputs

--- single_pulse_ml/make_dataset.py
+++ single_pulse_ml/make_dataset.py
@@ -1,56 +1,48 @@
 #!/usr/bin/env python
 
-import argparse
+import os
 import numpy as np
+import pandas as pd
 from mpi4py import MPI
-from baseband.helpers import sequentialfile as sf
 from FRBEvent import FRBEvent
+from baseband.helpers import sequentialfile as sf
 
-def create_argparser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--number',
-                        help='The # of simulations to create per input file',
-                        default=1, type=int)
-    parser.add_argument('-g', '--group',
-                        help='# of input files to group together as one file',
-                        default=10, type=int)
-    parser.add_argument('-f', '--files',
-                        help='The vdif files containing the background RFI',
-                        nargs='*')
-    parser.add_argument('-o', '--output',
-                        help='The directory to store the output in')
-    return parser.parse_args()
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
 
-def make_dataset(N, files, output):
-    comm = MPI.COMM_WORLD
-    rank = comm.rank
-    size = comm.size
-    count = 0
-    complete_metadata = []
-    while count + size < len(files):
-        if rank == 0:
-            data = files[count:count+size]
-        else:
-            data = None
-        data = comm.scatter(data, root=0)
-        metadata = []
-        for i in range(N):
-            event = FRBEvent(background=data)
-            name = data.split('/')[-1].replace('.vdif', '')
-            event.save(output + name + str(i) + '.npy')
-            metadata.append(event.parameters())
-        metadata = comm.gather(metadata, root=0)
-        complete_metadata.append(metadata)
-        count += size
-    df = pd.DataFrame(complete_metadata)
-    df.to_csv(output + 'metadata.csv', sep=',', index=False)
+scratch = '/scratch/r/rhlozek/rylan/'
+input_dir = scratch + 'aro_rfi/natasha/'
+output_dir = scratch + 'simulations/'
 
+n_repeats = 10
+group = 10
 
-if __name__ == "__main__":
-    args = create_argparser()
-    count = 0
-    while count + args.group < len(args.files):
-        grouped_files.append(np.sort(args.files[count:count+args.group]))
-        coutn += args.group
-    grouped_files = [sf.open(x) for x in grouped_files]
-    make_dataset(args.number, grouped_files, args.output)
+files = [input_dir + x for x in os.listdir(input_dir) if '.vdif' in x]
+files = np.sort(files)
+files = [files[x*group:(x*group)+group] for x in range(int(len(files)/group))]
+
+if rank == 0:
+    files = files[:size]
+else:
+    files = None
+
+data = comm.scatter(files, root=0)
+
+metadata = []
+for i in range(n_repeats):
+    event = FRBEvent(background=data)
+
+    letter = string.ascii_letters[i]
+    event.save(output_dir + event.input + '-{}.npy'.format(letter))
+
+    metadata.append(event.get_parameters())
+
+metadata = pd.DataFrame(metadata)
+
+complete_metadata = comm.gather(metadata, root=0)
+
+if rank == 0:
+    metadata = pd.concat(complete_metadata, ignore_index=True, axis=0)
+    metadata.to_csv(output_dir + 'metadata.csv', index=None)
+
